e019daadb4aaaafd9899d6e130e9193f09751c4a
sumeet.rana <sumeetsinh28@gmail.com>
2022-12-04 12:34:28 +0530

    middleware requireSignin for protected route created

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { signup, signin } = require('../controller/auth');
+const { signup, signin, requireSignin } = require('../controller/auth');
 
 const router = express.Router();
 
 router.post('/signin', signin);
 
 router.post('/signup', signup);
+
+router.post('/profile', requireSignin, (req, res) => {
+  res.json({
+    "user": "Profile"
+  })
+})
 
 module.exports = router;
